8704235f1564fc6e8f0691ffc99f8fb941a0dc49
Daniel <dnnxzz70@gmail.com>
2024-10-04 17:08:12 -0300

    modified:   .env 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-const port = 5173;
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
 
 export default defineConfig({
     plugins: [
@@ -11,12 +9,11 @@ export default defineConfig({
         }),
     ],
     server: {
-        // respond to all network requests
+        // Responde a todas as requisições de rede
         host: "0.0.0.0",
-        port: port,
+        port: 5173,
         strictPort: true,
-        // Defines the origin of the generated asset URLs during development,
-        // this will also be used for the public/hot file (Vite devserver URL)
-        origin: origin,
+        // Use a URL definida no .env
+        origin: process.env.VITE_APP_URL,
     },
 });
